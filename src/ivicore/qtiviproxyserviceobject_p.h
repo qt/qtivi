cc2c8c69443e5f0306f31e283faf298ca9f26c84
Marko Kangas <marko.kangas@theqtcompany.com>
2015-12-07 16:20:05 +0200

    Add missing "We mean it" comments to private headers
    Build warns about missing "We mean it" comments.
    
    Change-Id: I1c5d7a2409e81414950b127c3b3cd917916df43d
    Reviewed-by: Dominik Holland <dominik.holland@pelagicore.com>

--- src/ivicore/qtiviproxyserviceobject_p.h
+++ src/ivicore/qtiviproxyserviceobject_p.h
@@ -30,6 +30,17 @@
 #ifndef QTIVIPROXYSERVICEOBJECT_H
 #define QTIVIPROXYSERVICEOBJECT_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qtiviserviceobject.h"
 
 QT_BEGIN_NAMESPACE
--- src/ivicore/qtiviservicemanager_p.h
+++ src/ivicore/qtiviservicemanager_p.h
@@ -30,6 +30,17 @@
 #ifndef QTIVISERVICEMANAGER_P_H
 #define QTIVISERVICEMANAGER_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QAbstractListModel>
 #include <QVariantMap>
 #include <QStringList>
