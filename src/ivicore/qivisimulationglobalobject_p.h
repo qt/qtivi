533f5973e63d5c462bdffe273792063b624da56f
Dominik Holland <dominik.holland@pelagicore.com>
2019-04-26 11:58:10 +0200

    Add missing "We mean it" comment in private header
    Change-Id: I45e2684a8d7dd2eb7191f776becdb1e9038ca0f5
    Reviewed-by: Robert Griebl <robert.griebl@pelagicore.com>

--- src/ivicore/qivisimulationglobalobject_p.h
+++ src/ivicore/qivisimulationglobalobject_p.h
@@ -43,6 +43,17 @@
 #ifndef QIVISIMULATIONGLOBALOBJECT_P_H
 #define QIVISIMULATIONGLOBALOBJECT_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <qiviqmlconversion_helper.h>
 #include <QIviPagingModelInterface>
 
